ad759069c1d48aa6d11a6dd385e7e272c7683197
lubos-harasta <lubos.harasta@themama.ai>
2022-11-30 09:35:55 +0100

    wip: generic solution update

--- contracts/selfie/echidna/EchidnaSelfieGeneric.sol
+++ contracts/selfie/echidna/EchidnaSelfieGeneric.sol
@@ -17,11 +17,7 @@ contract SelfieDeployment {
 
     function deployContracts()
         external
-        returns (
-            SelfiePool,
-            SimpleGovernance,
-            DamnValuableTokenSnapshot
-        )
+        returns (SelfiePool, SimpleGovernance, DamnValuableTokenSnapshot)
     {
         // deploy contracts
         DamnValuableTokenSnapshot token;
@@ -44,31 +40,41 @@ contract EchidnaSelfieGeneric {
     uint256 private TOKENS_IN_POOL = 1_500_000 ether;
 
     uint256 private actionId; // to tract id of queued actions
-    uint256 private timestampOfActionQueued; // to track timestamp of queued actions
-
     uint256[] private actionsToBeCalled; // actions to be called in callback function
 
-    // TODO: no action as a parameter as well?
+    // all possible actions for callback
     enum Actions {
+        noActionToBeCalled,
         drainAllFunds,
         transferFrom,
         queueAction,
         executeAction
     }
-    uint256 private actionsLength = 4; // must correspond with the length of Actions
+    uint256 private actionsLength = 5; // must correspond with the length of Actions
 
+    // function's parameters to be defined by Echidna
+    struct FunctionParameters {
+        bool amountTransferForTransferFunction;
+        bool amountTransferForPayload;
+        bool weiInQueueAction;
+        bool payloadInQueueAction;
+    }
+    FunctionParameters functionParametersSet; // to check if parameter has been already set
+
+    // queueAction payloads to be created by Echidna
     enum Payloads {
         emptyPayload,
         drainAllFunds,
         transferFrom
     }
     uint256 private payloadsLength = 3; // must correspond with the length of Payloads
-    uint256 private _payloadSet;
-    bytes private _payload;
+    uint256 private _payloadSet; // to know which payload has been set (logging purposes)
+    bytes private _payload; // current payload
 
-    uint256 private _amountToTransferForPayload;
-    uint256 private _amountToTransferForTransferFunction;
+    // other parameters to be set by echidna
+    uint256 private _amountToTransferForPayloadForQueueAction;
     uint256 private _weiAmountForQueueAction;
+    uint256 private _amountToTransferForTransferFunction;
 
     SelfiePool pool;
     SimpleGovernance governance;
@@ -107,94 +113,6 @@ contract EchidnaSelfieGeneric {
     }
 
     /**
-     * @notice Echidna populates actionsToBeCalled by a numbers representing functions
-     * to be called during callback in receiveTokens()
-     * @param num a number represing a function to be called
-     */
-    function pushActionToCallback(uint256 num) external {
-        num = num % actionsLength;
-        actionsToBeCalled.push(num);
-    }
-
-    /**
-     * @notice an action to be called
-     * @param _num a number representing the action to be called
-     */
-    function callAction(uint256 _num) internal {
-        require(0 <= _num && _num < actionsLength, "Out of range");
-        // drain all funds
-        if (_num == uint256(Actions.drainAllFunds)) {
-            drainAllFunds();
-        }
-        // transfer
-        if (_num == uint256(Actions.transferFrom)) {
-            transferFrom();
-        }
-        // queue an action
-        if (_num == uint256(Actions.queueAction)) {
-            try this.queueAction() {} catch {
-                revert("queueAction unsuccessful");
-            }
-        }
-        // execute an action
-        if (_num == uint256(Actions.executeAction)) {
-            try this.executeAction() {} catch {
-                revert("queueAction unsuccessful");
-            }
-        }
-    }
-
-    function setPayload(uint256 _num) external {
-        require(0 <= _num && _num < actionsLength, "Out of range");
-        _payloadSet = _num;
-        // empty payload
-        if (_num == uint256(Payloads.emptyPayload)) {
-            _payload = "";
-        }
-        // drainAllFunds;
-        if (_num == uint256(Payloads.drainAllFunds)) {
-            _payload = abi.encodeWithSignature(
-                "drainAllFunds(address)",
-                address(this)
-            );
-        }
-        // transfer
-        if (_num == uint256(Payloads.transferFrom)) {
-            _payload = abi.encodeWithSignature(
-                "transferFrom(address,address,uint256)",
-                address(pool),
-                address(this),
-                _amountToTransferForPayload
-            );
-        }
-    }
-
-    /**
-     * @notice set amount to transfer into payload
-     */
-    function setAmountToTransferForPayload(uint256 _amount) external {
-        _amountToTransferForPayload = _amount;
-    }
-
-    /**
-     * @notice set amount to transfer into payload
-     */
-    function setAmountToTransferForTransfer(uint256 _amount) external {
-        _amountToTransferForTransferFunction = _amount;
-    }
-
-    /**
-     * @notice set amount of wei for transferFrom
-     */
-    function setWeiAmountForQueueAction(uint256 _amount) external {
-        _weiAmountForQueueAction = _amount;
-    }
-
-    /////////////
-    // ACTIONS //
-    /////////////
-
-    /**
      * @notice actions to be called once receiveTokens() is called
      */
     function callbackActions() internal {
@@ -208,56 +126,183 @@ contract EchidnaSelfieGeneric {
         }
     }
 
-    function queueAction() public {
-        // take a snaphost first as it is needed in queueAction()
-        token.snapshot();
-        // queue the action
-        actionId = governance.queueAction(
-            address(pool),
-            _payload,
-            _weiAmountForQueueAction
-        );
-        // set timestamp when action was queued (needed to pass the requirement in the executeAction)
-        timestampOfActionQueued = block.timestamp;
-    }
-
-    function executeAction() public {
-        // it does not make sense to call executeAction if the requirment is not met
-        require(
-            block.timestamp >=
-                timestampOfActionQueued + ACTION_DELAY_IN_SECONDS,
-            "Time for action execution has not passed yet"
-        );
-        governance.executeAction(actionId);
-    }
-
     /**
-     * @notice this function should always revert as we should not be able
-     * to drain all funds from pool
+     * @notice an action to be called
+     * @param _num a number representing the action to be called
      */
+    function callAction(uint256 _num) internal {
+        // no action
+        if (_num == uint256(Actions.noActionToBeCalled)) {
+            // just do nothing
+        }
+        // drain all funds
+        if (_num == uint256(Actions.drainAllFunds)) {
+            drainAllFunds();
+        }
+        // transfer funds
+        if (_num == uint256(Actions.transferFrom)) {
+            transferFrom();
+            // reset setter after the call
+            functionParametersSet.amountTransferForTransferFunction = false;
+        }
+        // queue an action
+        if (_num == uint256(Actions.queueAction)) {
+            // reset setters after the call
+            functionParametersSet.payloadInQueueAction = false;
+            functionParametersSet.weiInQueueAction = false;
+            if (_payloadSet == uint256(Payloads.transferFrom)) {
+                functionParametersSet.amountTransferForPayload = false;
+            }
+            try this.queueAction() {} catch {
+                revert("queueAction unsuccessful");
+            }
+        }
+        // execute an action
+        if (_num == uint256(Actions.executeAction)) {
+            try this.executeAction() {} catch {
+                revert("queueAction unsuccessful");
+            }
+        }
+    }
+
+    //////////////////////////////////////////////
+    // PUSHERS AND SETTTERS OF CALLBACK ACTIONS //
+    //////////////////////////////////////////////
+
+    ////////////////////////
+    // 1: drainAllFunds() //
     function drainAllFunds() public {
-        uint256 _poolBalance = token.balanceOf(address(pool));
         pool.drainAllFunds(address(this));
-        uint256 _poolBalanceAfter = token.balanceOf(address(pool));
-        require(
-            _poolBalanceAfter > _poolBalance,
-            "Draining all funds has been unsuccessful"
-        );
     }
 
-    /**
-     * @notice this function should always revert as we should not be able
-     * to transfer token from pool
-     */
+    function pushDrainAllFundsToCallback() external {
+        actionsToBeCalled.push(uint256(Actions.drainAllFunds));
+    }
+
+    ///////////////////////
+    // 2: transferFrom() //
+    // -> pushers: pushTransferFromToCallback()
+    // -> setters: setAmountToTransferForTransfer()
     function transferFrom() public {
-        uint256 _poolBalance = token.balanceOf(address(pool));
         token.transferFrom(
             address(pool),
             address(this),
             _amountToTransferForTransferFunction
         );
-        uint256 _poolBalanceAfter = token.balanceOf(address(pool));
-        require(_poolBalanceAfter > _poolBalance, "Transfer unsuccessful");
+    }
+
+    function pushTransferFromToCallback() external {
+        require(
+            functionParametersSet.amountTransferForTransferFunction,
+            "Amount to Transfer has not been defined"
+        );
+        actionsToBeCalled.push(uint256(Actions.transferFrom));
+    }
+
+    function setAmountToTransferForTransfer(uint256 _amount) external {
+        _amountToTransferForTransferFunction = _amount;
+        functionParametersSet.amountTransferForTransferFunction = true;
+    }
+
+    //////////////////////
+    // 3: queueAction() //
+    // -> pushers: pushQueueActionToCallback()
+    // -> setters: setWeiAmountForQueueAction(), setPayload(), setAmountToTransferForPayload()
+    function queueAction() public {
+        require(
+            address(this).balance >= _weiAmountForQueueAction,
+            "Not sufficient account balance to queue an action"
+        );
+        // take a snaphost first as it is needed in queueAction()
+        token.snapshot();
+        // queue the action;
+        actionId = governance.queueAction(
+            address(pool),
+            _payload,
+            _weiAmountForQueueAction
+        );
+    }
+
+    function pushQueueActionToCallback() external {
+        require(
+            functionParametersSet.payloadInQueueAction == true,
+            "Payload must be specified."
+        );
+        require(
+            functionParametersSet.weiInQueueAction == true,
+            "Amount of WEI must be specified."
+        );
+        actionsToBeCalled.push(uint256(Actions.queueAction));
+    }
+
+    function setWeiAmountForQueueAction(uint256 _amount) external {
+        require(
+            address(this).balance >= _amount,
+            "Not sufficient account balance"
+        );
+        // _amount = _amount % address(this).balance;
+        _weiAmountForQueueAction = _amount;
+        // _weiAmountForQueueAction = 0;
+        functionParametersSet.weiInQueueAction = true;
+    }
+
+    function setPayload(uint256 _num) external {
+        _num = _num % actionsLength;
+        // update state variables
+        _payloadSet = _num; // to know which payload use in queueAction()
+        functionParametersSet.payloadInQueueAction = true;
+        // create payloads:
+        //  - empty payload
+        if (_num == uint256(Payloads.emptyPayload)) {
+            _payload = "";
+        }
+        //  - drainAllFunds
+        if (_num == uint256(Payloads.drainAllFunds)) {
+            _payload = abi.encodeWithSignature(
+                "drainAllFunds(address)",
+                address(this)
+            );
+        }
+        //  - transfer
+        if (_num == uint256(Payloads.transferFrom)) {
+            require(
+                functionParametersSet.amountTransferForPayload == true,
+                "amountTransferForPayload has not been set"
+            );
+            _payload = abi.encodeWithSignature(
+                "transferFrom(address,address,uint256)",
+                address(pool),
+                address(this),
+                _amountToTransferForPayloadForQueueAction // needs to be set
+            );
+        }
+    }
+
+    function setAmountToTransferForPayload(uint256 _amount) external {
+        _amountToTransferForPayloadForQueueAction = _amount;
+        functionParametersSet.amountTransferForPayload = true;
+    }
+
+    ////////////////////////
+    // 4: executeAction() //
+    function executeAction() public {
+        // get data related to the action to be executed
+        (, , uint256 weiAmount, uint256 proposedAt, ) = governance.actions(
+            actionId
+        );
+        require(
+            address(this).balance >= weiAmount,
+            "Not sufficient account balance to execute the action"
+        );
+        require(
+            block.timestamp >= proposedAt + ACTION_DELAY_IN_SECONDS,
+            "Time for action execution has not passed yet"
+        );
+        governance.executeAction{value: weiAmount}(actionId); // TODO: Add if statement for weiAmount == 0?
+    }
+
+    function pushExecuteActionToCallback() external {
+        actionsToBeCalled.push(uint256(Actions.executeAction));
     }
 
     /////////////
@@ -305,7 +350,7 @@ contract EchidnaSelfieGeneric {
             emit PayloadSet("transferFrom(address,address,uint256)");
             emit PayloadVariable(
                 "_amountToTransfer",
-                _amountToTransferForPayload
+                _amountToTransferForPayloadForQueueAction
             );
         }
     }
